168d9e66ec3e368e3273f115c9c7b9565e600680
James Ciclitira <jciclitira@gmail.com>
2018-09-20 13:32:53 +0200

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,7 @@ class Landing extends React.Component {
     return (
   <section >
           <main className="dib v-top pr0 pa0 tl-ns tc vh-100">
-          <h1 className='f1-l mt5-ns pt3-ns pb2 lh-copy'><span className='purple'>Hello, I’m James.</span> <br/>Berlin based Product Designer.</h1>
+          <h1 className='f1-l mt5-ns pt3-ns pb2 lh-copy'>Hello, I’m James.<br/>Berlin based Product Designer.</h1>
           <p className='secondary-text measure lh-copy' style={{fontSize: '18px'}}>Passionate in the creation of meaningful digital experiences with a focus on user centric design. Interesting in the design of engaging and meaningful products based on users needs and data driven research.  </p>
           <Social/>
           </main>
