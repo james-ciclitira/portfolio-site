7fba6d378adf0ddbc3f8592696edf14766298c7f
james-ciclitira <jciclitira@gmail.com>
2019-06-26 10:16:41 +0200

    copyupdate

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,8 +6,14 @@ class Landing extends React.Component {
     return (
   <section >
           <main className="dib v-top pr0 pa0 tl-ns tc vh-100">
-          <h1 className='f1-l mt5-ns pt3-ns pb2 lh-copy'>Hello, I’m James.<br/>Berlin based Product Designer.</h1>
-          <p className='secondary-text measure lh-copy' style={{fontSize: '18px'}}>Passionate in the creation of meaningful digital experiences with a focus on user centric design. Interesting in the design of engaging and meaningful products based on users needs and data driven research.  </p>
+          <h1 className='f1-l mt5-ns pt3-ns pb2 lh-copy'>Hello I’m James,<br/>a Berlin based Product Designer.</h1>
+          <p className='secondary-text mw7 lh-copy' style={{fontSize: '18px'}}>Passionate about the creation of meaningful digital experiences with a focus on user-centric design.
+
+Interesting in the design of engaging and meaningful products based on users needs and data-driven research.
+
+I’m always happy to have a chat so please don’t be shy and drop me a message or check out my résumé.
+
+Have a nice day!</p>
           <Social/>
           </main>
 </section>
