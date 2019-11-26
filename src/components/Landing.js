98c3944109d591f3a68cd092fef93c786ab06ed5
James Ciclitira <jamesciclitira@Jamess-MacBook-Pro.local>
2019-11-26 12:55:04 +0100

    header

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,15 +6,13 @@ class Landing extends React.Component {
     return (
   <section >
           <main className="dib v-top tl-ns tc vh-100 dib">
-          <h1 className='f1-l mt5-ns pt3-ns pb0 lh-copy fw4'>Hello I’m James,<br/>Digital product & experience designer</h1>
+          <h1 className='f1-l mt5-ns pt3-ns pb0 lh-copy fw4'>Hello I’m James,<br/>Product & experience designer</h1>
 
 
 
-          <p className='secondary-text mw8 pr6 lh-copy' style={{fontSize: '18px'}}>
+          <p className='pr6 mr2 lh-copy' style={{fontSize: '20px'}}>
 
-Passionate about the creation of meaningful digital experiences with a focus on user-centric design. Interests in sustainability, education, healthcare and universal design.  
-I’m always happy to have a chat so please don’t be shy.
-Enjoy your day!
+Passionate about the creation of meaningful digital experiences with a focus on user-centric design. Interests in Clean tech,  Sustainability, Education, Healthcare and Accessibility.
 </p>
           <Social/>
           </main>
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -19,22 +19,13 @@ const Header = () =>
 const TemplateWrapper = ({ children }) =>
   <div>
     <Helmet
-      title="James Ciclitira product designer"
+      title="James Ciclitira - Product designer"
       link={[
-      { rel: 'apple-touch-icon', sizes: '57x57', href: 'favicon/apple-icon-57x57.png' },
-      { rel: 'apple-touch-icon', sizes: '60x60', href: 'favicon/apple-icon-60x60.png' },
-      { rel: 'apple-touch-icon', sizes: '72x72', href: 'favicon/apple-icon-72x72.png' },
-      { rel: 'apple-touch-icon', sizes: '76x76', href: 'favicon/apple-icon-76x76.png' },
-      { rel: 'apple-touch-icon', sizes: '114x114', href: 'favicon/apple-icon-114x114.png' },
-      { rel: 'apple-touch-icon', sizes: '120x120', href: 'favicon/apple-icon-120x120.png' },
-      { rel: 'apple-touch-icon', sizes: '144x144', href: 'favicon/apple-icon-144x144.png' },
-      { rel: 'apple-touch-icon', sizes: '152x152', href: 'favicon/apple-icon-152x152.png' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: 'favicon/apple-icon-180x180.png' },
-      { rel: 'icon', type:'image/png', sizes: '192x192' , href: 'favicon/android-icon-192x192.png' },
-      { rel: 'icon', type:'image/png', sizes: '32x32', href: 'favicon/favicon-32x32.png' },
-      { rel: 'icon', type:'image/png', sizes: '96x96', href: 'favicon/favicon-96x96.png' },
-      { rel: 'icon', type:'image/png', sizes: '16x16', href: 'favicon/favicon-16x16.png' },
-      { rel: 'manifest', href: 'favicon/manifest.json'}
+
+{ rel: 'apple-touch-icon', size: '180x180', href: 'favicon_io/apple-touch-icon.png' },
+{ rel: 'icon', type:'image/png',  size: '32x32', href: 'favicon_io/favicon-32x32.png' }, 
+{ rel: 'icon',  type:'image/png',  size: '16x16', href: 'favicon_io/favicon-16x16.png' },
+{ rel: 'manifest', href:'favicon_io/site.webmanifest' },    
         ]}
       meta={[
         { name: 'description', content: 'Digital Product Designer based in Berlin. Creatively resolving problems & building amazing user experiences, specialising in Mobile & Web design' },
