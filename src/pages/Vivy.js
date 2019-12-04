bd028482024b0d4f6feb6903decc6b8543c98eb1
James Ciclitira <jamesciclitira@Jamess-MacBook-Pro.local>
2019-12-04 17:12:20 +0100

    vivy

--- src/pages/Vivy.js
+++ src/pages/Vivy.js
@@ -22,9 +22,10 @@ job=' User interviews, diary studies, card sorting, storyboarding, wireframing, 
     <img className='w-100' alt='onboarding screens' src='images/vivy/onboarding.png' />
 
 
-<h3>Case Study: Doctors portal</h3>	
+<h1>Case Study: <br/><br/>
+Document storage and sharing</h1>	
 
-<p>26 November 2019</p>
+<p>November 2018</p>
 
 <p className='pb3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis venenatis elit sed felis elementum egestas. Nullam placerat maximus elit pharetra rutrum. Proin eleifend lorem lacus, vel cursus dolor tincidunt eu. Cras metus sem, lacinia eu vestibulum vitae, semper dictum leo. Vivamus quis tellus ex. Donec congue vestibulum mi, non eleifend massa gravida a. Vivamus porttitor iaculis quam, nec efficitur augue consequat at. Aenean lorem lectus, ultricies vitae consectetur et, imperdiet et odio. Nulla vel ipsum tempus, pellentesque orci sed, commodo risus. Quisque eleifend, purus ac pulvinar fermentum, nulla magna iaculis lorem, at aliquam nisl ante id est. Donec aliquam lobortis sapien ac tincidunt.
 
@@ -32,12 +33,39 @@ Vestibulum rutrum vel justo vel pulvinar. Etiam pellentesque, nulla sed vestibul
 
 </p>
 
+
+
+
+    <img className='w-100' alt='Doctors portal' src='images/vivy/request.png' />
+    <img className='w-100' alt='Doctors portal' src='images/vivy/share.png' />
+    <img className='w-100' alt='Doctors portal' src='images/vivy/status.png' />
+  	<img className='w-100' alt='Doctors portal' src='images/vivy/dr.png' />
+
 <a className='bg-black white pv2 ph3 br2'  target='blank' href='https://doc-request.netlify.com/'>Prototype</a>
 
+  </div>
+</section>
 
-  <img className='w-100' alt='Doctors portal' src='images/vivy/dr.png' />
 
-  
+
+
+
+
+<section className='mw8 center pt5'>
+  <div className=' ma0 ph2'>
+
+
+
+<h1>Case Study: <br/><br/> Invoice management</h1>	 
+
+<p>July 2018</p>
+
+<p className='pb3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis venenatis elit sed felis elementum egestas. Nullam placerat maximus elit pharetra rutrum. Proin eleifend lorem lacus, vel cursus dolor tincidunt eu. Cras metus sem, lacinia eu vestibulum vitae, semper dictum leo. Vivamus quis tellus ex. Donec congue vestibulum mi, non eleifend massa gravida a. Vivamus porttitor iaculis quam, nec efficitur augue consequat at. Aenean lorem lectus, ultricies vitae consectetur et, imperdiet et odio. Nulla vel ipsum tempus, pellentesque orci sed, commodo risus. Quisque eleifend, purus ac pulvinar fermentum, nulla magna iaculis lorem, at aliquam nisl ante id est. Donec aliquam lobortis sapien ac tincidunt.
+
+Vestibulum rutrum vel justo vel pulvinar. Etiam pellentesque, nulla sed vestibulum tincidunt, libero urna ornare nunc, ut porttitor erat elit quis tortor. Pellentesque gravida neque ante, at vulputate magna sodales ut. Cras maximus pharetra tortor, et eleifend leo cursus vitae. Mauris quis nibh ultricies, vestibulum tortor eu, tempus orci. Morbi interdum, ex ac semper facilisis, neque nisl placerat justo, ut euismod nisl nibh nec leo. Praesent porttitor enim id nunc congue, a tempus dui suscipit. Sed ornare eros ac purus tempor volutpat. In eu odio eu nibh blandit scelerisque eget vel nunc.
+
+</p>
+
 
 
 
