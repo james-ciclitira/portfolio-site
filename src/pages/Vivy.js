0ee7d072ead57587f1f2a6da032c91a8ed4ddcd7
James Ciclitira <jamesciclitira@Jamess-MacBook-Pro.local>
2019-12-04 16:13:54 +0100

    vivy-work

--- src/pages/Vivy.js
+++ src/pages/Vivy.js
@@ -10,26 +10,34 @@ const Vivy = () =>
 <div className='pt5'>
 <CaseStudyAbout
 about='Vivy is a electronic health record, using end-to-end encryption which allows customers to securely store and share medical documents'
-role='As one of the initial designers, I supported by helping to establish the foundation of Vivy by shaping the product vision and pushing to prioritize reaserch and the customer experience.'
+role='As one of the initial designers, I supported by helping to establish the foundation of Vivy; shaping the product vision and pushing to prioritize research and the customer experience.'
 
 clients='Product Designer '
 job=' User interviews, diary studies, card sorting, storyboarding, wireframing, Interface design, Usability studies etc..'
 />
 
 <section className='mw8 center'>
-  <div className='pv4 ma0 ph2'>
+  <div className=' ma0 ph2'>
+
+    <img className='w-100' alt='onboarding screens' src='images/vivy/onboarding.png' />
 
 
-  <img className='w-40 db center' style={{marginTop: '-68px'}} alt='loading' src='images/soon.gif' />
+<h3>Case Study: Doctors portal</h3>	
+
+<p>26 November 2019</p>
+
+<p className='pb3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis venenatis elit sed felis elementum egestas. Nullam placerat maximus elit pharetra rutrum. Proin eleifend lorem lacus, vel cursus dolor tincidunt eu. Cras metus sem, lacinia eu vestibulum vitae, semper dictum leo. Vivamus quis tellus ex. Donec congue vestibulum mi, non eleifend massa gravida a. Vivamus porttitor iaculis quam, nec efficitur augue consequat at. Aenean lorem lectus, ultricies vitae consectetur et, imperdiet et odio. Nulla vel ipsum tempus, pellentesque orci sed, commodo risus. Quisque eleifend, purus ac pulvinar fermentum, nulla magna iaculis lorem, at aliquam nisl ante id est. Donec aliquam lobortis sapien ac tincidunt.
+
+Vestibulum rutrum vel justo vel pulvinar. Etiam pellentesque, nulla sed vestibulum tincidunt, libero urna ornare nunc, ut porttitor erat elit quis tortor. Pellentesque gravida neque ante, at vulputate magna sodales ut. Cras maximus pharetra tortor, et eleifend leo cursus vitae. Mauris quis nibh ultricies, vestibulum tortor eu, tempus orci. Morbi interdum, ex ac semper facilisis, neque nisl placerat justo, ut euismod nisl nibh nec leo. Praesent porttitor enim id nunc congue, a tempus dui suscipit. Sed ornare eros ac purus tempor volutpat. In eu odio eu nibh blandit scelerisque eget vel nunc.
+
+</p>
+
+<a className='bg-black white pv2 ph3 br2'  target='blank' href='https://doc-request.netlify.com/'>Prototype</a>
 
 
+  <img className='w-100' alt='Doctors portal' src='images/vivy/dr.png' />
 
-  <img className='w-100 dn' alt='invoice-management' src='images/vivy/invoice management.png' />
-
-  <img className='w-100 dn' alt='Dr-dashboard' src='images/vivy/dashboard.png' />
-
-
-<a className='w-100 dn' target='blank' href='https://doc-request.netlify.com/' >pototpe </a>
+  
 
 
 
