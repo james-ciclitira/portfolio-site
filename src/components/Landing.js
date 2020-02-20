4c64b5ab50a28a2fd396be8d8884f3c2f8acf2b7
Adam <Adam@James.fritz.box>
2020-02-20 15:44:00 +0100

    landing change

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,8 +8,7 @@ class Landing extends React.Component {
           <main className="dib v-top tl-ns tc vh-100 dib">
           <h1 className='f1-l mt5-ns mb2 pt3-ns pb0 lh-copy fw4'>Hello I’m James,<br/>Product & Industrial Designer</h1>
           <p className='pr6-l pr0 mr2-l mr0 lh-copy' style={{fontSize: '20px'}}>
-
-Passionate about the creation of meaningful experiences with a focus on user-centric design & research. Interests in education, sustainability and accessibility.
+Passionate in creative problem solving with a focus on user-centric design methods only by understanding user behaviors, needs, and motivations can meaningful experiences be created. Personal interests in education, psychology, sustainability and accessibility.
 </p>
           <Social/>
           </main>
