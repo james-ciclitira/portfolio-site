7b1e4d83a07cc595c7263c4cae0bec9cd9fba881
Adam <Adam@James.fritz.box>
2020-02-21 12:42:12 +0100

    typos

--- src/components/Work.js
+++ src/components/Work.js
@@ -13,9 +13,9 @@ class Work extends React.Component {
     <section>
         <h2 className='mb5'>Recent projects</h2>
 
-        <WorkTile title='Coup' info='Mobile application' summary='Coup is a electric scooter sharing service operating in Berlin, Madrid & Paris!' img='images/preview/coup.jpg' link='Coup'/>
-        <WorkTile title='Vivy' info='Mobile application' summary='Vivy is a digital health record. Enabling user to securely store encrypted medical documents on a smart phone' img='images/preview/vivy.jpg' link='Vivy'/>
-        <WorkTile title='CrowdScores' info='Mobile application' summary='CrowdScores brings you the fastest live scores. It is a social network which allows fans to share in-match events and opinion. Running on both iOS and Android.' img='images/preview/crowdscores-app.jpg' link='/CrowdScores'/>
+        <WorkTile title='Coup' info='Mobile application' summary='Coup is an electric scooter sharing service operating in Berlin, Madrid & Paris!' img='images/preview/coup.jpg' link='Coup'/>
+        <WorkTile title='Vivy' info='Mobile application' summary='Vivy is a digital health record. Enabling users to securely store encrypted medical documents on a smart phone.' img='images/preview/vivy.jpg' link='Vivy'/>
+        <WorkTile title='CrowdScores' info='Mobile application' summary='CrowdScores brings you the fastest live scores. It is a social network which allows fans to share in-match events and opinions.' img='images/preview/crowdscores-app.jpg' link='/CrowdScores'/>
         <WorkTile title='YunoJuno' info='Web application' summary='YunoJuno connects the best freelancers with great companies. Employers work directly with freelancers with YunoJuno managing contact and payment.' img='images/preview/yunojuno.jpg' link='YunoJuno'/>
         <WorkTile title='QuidCo' info='Mobile application' summary='Quidco is the number one cashback service. Partnering with retailers to help consumers earn cashback every year.' img='images/preview/quidco.jpg' link='Quidco'/>
         <WorkTile title='Suggestv' info='Web application' summary='Suggestv is the inventor of semantic video distribution. Suggestv brings together content sellers and publishes creating a marketplace for online video.' img='images/preview/suggestv-platfom.jpg' link='Suggestv'/>
