62d6f680cca09f493776479d08b4c337ddccb068
Adam <Adam@James.fritz.box>
2020-06-16 13:43:26 +0200

    wording

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,7 @@ class Landing extends React.Component {
     return (
 <section className="dib v-top tl-ns tc vh-100 dib relative">
 
-          <h1 className='f1-l mt5-ns mb2 pt3-ns pb0 lh-copy fw5'>Hello I'm Jimmy,<br/>Digital Product Designer</h1>
+          <h1 className='f1-l mt5-ns mb2 pt3-ns pb0 lh-copy fw5'>Hello I'm James,<br/>Digital Product Designer</h1>
           <p className='pr6-l pr0 mr2-l mr0 lh-copy' style={{fontSize: '20px'}}>
 Passionate about creative problem solving and creating meaningful  experiences. Interested in education, human-to-computer interaction, social change, sustainability, and accessibility.</p>
 <Social/>
