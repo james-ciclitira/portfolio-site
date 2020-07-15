cfb2e02b98889434d85411be0ea02594dd133d5c
Adam <Adam@James.fritz.box>
2020-07-15 09:54:53 +0200

    landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,7 +8,7 @@ class Landing extends React.Component {
 
           <h1 className='f1-l mt5-ns mb2 pt3-ns pb0 lh-copy fw5'>Hello I'm James,<br/>Digital Product Designer</h1>
           <p className='pr6-l pr0 mr2-l mr0 lh-copy' style={{fontSize: '20px'}}>
-Passionate about creative problem solving and creating meaningful  experiences. Interested in education, human-to-computer interaction, social change, sustainability, and accessibility.</p>
+Berlin-based Product Designer creating meaningful digital experiences. I am passionate about creative problem-solving, education, social change, sustainability, and accessibility.</p>
 <Social/>
 
 
