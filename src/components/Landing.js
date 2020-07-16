9b526e44303b1a35ae5ef643a7490bd1db2fb2fb
Adam <Adam@James.fritz.box>
2020-07-16 10:51:34 +0200

    button

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,13 +4,15 @@ import Social from './Social'
 class Landing extends React.Component {
   render() {
     return (
-<section className="dib v-top tl-ns tc vh-100 dib relative">
+<section className="dib v-top tl-ns tc vh-100 dib relative ">
 
-          <h1 className='f1-l mt5-ns mb2 pt3-ns pb0 lh-copy fw5'>Hello I'm James,<br/>Digital Product Designer</h1>
+          <h1 className='f1-l mt5-ns mb3 pt3-ns pb0 lh-copy fw5'>Hello I'm James,<br/>Digital Product Designer</h1>
           <p className='pr6-l pr0 mr2-l mr0 lh-copy' style={{fontSize: '20px'}}>
-Berlin-based Product Designer creating meaningful digital experiences. I am passionate about creative problem-solving, education, social change, sustainability, and accessibility.</p>
-<Social/>
+Berlin based Product Designer creating meaningful digital experiences. <br/> Passionate about education, social change, sustainability, and accessibility.</p>
 
+<div className='mt5 pt2'>
+<a href="mailto:jciclitira@gmail.com" className='f6 grow no-underline bn ph4 pv3 dib-ns dn mr4 bg-black white pointer'>Say hello</a><Social/>
+</div>
 
 </section>
     )
