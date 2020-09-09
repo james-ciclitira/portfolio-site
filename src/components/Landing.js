642a8480431ed3b4e3c9c72a5c8d88a452864859
james-ciclitira <jciclitira@gmail.com>
2020-09-09 08:48:51 +0200

    landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,9 +6,9 @@ class Landing extends React.Component {
     return (
 <section className="dib v-top tl-ns tc vh-100 dib relative ">
 
-          <h1 className='f1-l mt5-ns mb3 pt3-ns pb0 lh-copy fw5'>Hello I'm James,<br/>Digital Product Designer</h1>
+          <h1 className='f1-l mt5-ns mb3 pt3-ns pb0 lh-copy fw5'>Hello I'm James,<br/>Product & Industrial Designer</h1>
           <p className='pr6-l pr0 mr2-l mr0 lh-copy' style={{fontSize: '20px'}}>
-Berlin based Product Designer creating meaningful digital experiences. <br/> Passionate about education, social change, sustainability, and accessibility.</p>
+Berlin based Product Designer creating meaningful experiences. <br/> Passionate about education,  sustainability, social change and accessibility.</p>
 
 <div className='mt5 pt2'>
 <Social/>
